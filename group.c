3512ab077efe26786439c8a59d2b216267155f56
Cian-M <cian.mcginn@gmail.com>
2019-09-19 14:45:10 -0600

    Removed starter print statement

--- group.c
+++ group.c
@@ -10,7 +10,6 @@ void printCian() {
 	printf("Major: Computer Science\n");
 	printf("Hobbies: Ultimate Frisbee, SSBM, other video games, programming\n");
 	printf("Hometown: Seattle\n");
-	printf("FIXME: print name, year, major, hobbies, where you grew up, and anything else interesting.");
 }
 
 void printAndrew() {
