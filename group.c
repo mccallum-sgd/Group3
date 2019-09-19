81f0c4febef02196f90d52bc983f5bf4d66126ad
Sean McCallum <mccallum.sgd@gmail.com>
2019-09-19 14:39:07 -0600

    Created group.c

---
+++ group.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+void printSean() {
+	printf("FIXME: print name, year, major, hobbies, where you grew up, and anything else interesting.");
+}
+
+void printCian() {
+	printf("FIXME: print name, year, major, hobbies, where you grew up, and anything else interesting.");
+}
+
+void printAndrew() {
+	printf("FIXME: print name, year, major, hobbies, where you grew up, and anything else interesting.");
+}
+
+void printHalle() {
+	printf("FIXME: print name, year, major, hobbies, where you grew up, and anything else interesting.");
+}
+
+int main(void) {
+	printSean();
+	printCian();
+	printAndrew();
+	printHalle();
+}
